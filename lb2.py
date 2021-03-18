4d22278da5ddf550262e3bb5b636a09ec3b5fd15
Rustamov Arsen <43421600+myusernameisalreadytakenn@users.noreply.github.com>
2021-03-18 21:04:27 +0200

    Update lb2.py

--- lb2.py
+++ lb2.py
@@ -7,67 +7,72 @@ def Fuva(a, b):
         return a / b
     else:
         return b / a
-m=5
-x1min,x1max = 15,45
-x2min,x2max = -15,45
-y_max = (30 - 20) * 10
-y_min = (20 - 20) * 10
+while True:
+    m=5
+    x1min,x1max = 15,45
+    x2min,x2max = -15,45
+    y_max = (30 - 20) * 10
+    y_min = (20 - 20) * 10
 
 
-y=[[random.randint(y_min, y_max) for j in range(m)] for i in range(3)]
-ysrednie=[]
+    y=[[random.randint(y_min, y_max) for j in range(m)] for i in range(3)]
+    ysrednie=[]
 
-for i in range(len(y)):
-    SrednieY1 = 0
-    for j in y[i]:
-        SrednieY1 +=j
-    ysrednie.append(SrednieY1/m)
-
-
-
-Dispersia=[]
-Dispersia.append(np.var(y[0]))
-Dispersia.append(np.var(y[1]))
-Dispersia.append(np.var(y[2]))
-
-
-sigma = math.sqrt((2 * (2 * m - 2)) / (m * (m - 4)))
-
-
-Fuv=[]
-Fuv.append(Fuva(Dispersia[0], Dispersia[1]))
-Fuv.append(Fuva(Dispersia[2], Dispersia[0]))
-Fuv.append(Fuva(Dispersia[2], Dispersia[1]))
+    for i in range(len(y)):
+        SrednieY1 = 0
+        for j in y[i]:
+            SrednieY1 +=j
+        ysrednie.append(SrednieY1/m)
 
 
 
-Ouv=[]
-Ouv.append(((m - 2) / m) * Fuv[0])
-Ouv.append(((m - 2) / m) * Fuv[1])
-Ouv.append(((m - 2) / m) * Fuv[2])
+    Dispersia=[]
+    Dispersia.append(np.var(y[0]))
+    Dispersia.append(np.var(y[1]))
+    Dispersia.append(np.var(y[2]))
 
 
-Ruv=[]
-Ruv.append((abs(Ouv[0] - 1) / sigma))
-Ruv.append((abs(Ouv[1] - 1) / sigma))
-Ruv.append((abs(Ouv[2] - 1) / sigma))
+    sigma = math.sqrt((2 * (2 * m - 2)) / (m * (m - 4)))
 
 
-kr = 2
-for i in range(len(Ruv)):
-    if Ruv[i] > kr:
-        Proverka="Недостатня кількість єксперементів"
-    else:
-        Proverka=("Дисперсія однорідна")
+    Fuv=[]
+    Fuv.append(Fuva(Dispersia[0], Dispersia[1]))
+    Fuv.append(Fuva(Dispersia[2], Dispersia[0]))
+    Fuv.append(Fuva(Dispersia[2], Dispersia[1]))
+
+
+
+    Ouv=[]
+    Ouv.append(((m - 2) / m) * Fuv[0])
+    Ouv.append(((m - 2) / m) * Fuv[1])
+    Ouv.append(((m - 2) / m) * Fuv[2])
+
+
+    Ruv=[]
+    Ruv.append((abs(Ouv[0] - 1) / sigma))
+    Ruv.append((abs(Ouv[1] - 1) / sigma))
+    Ruv.append((abs(Ouv[2] - 1) / sigma))
+
+    PerevirkDuspersii=[]
+    kr = 2
+    for i in range(len(Ruv)):
+        if Ruv[i] > kr:
+            Proverka="Недостатня кількість єксперементів"
+            PerevirkDuspersii.append(0)
+        else:
+            Proverka=("Дисперсія однорідна")
+            PerevirkDuspersii.append(1)
+    if PerevirkDuspersii==[1,1,1]:
+        break
 
 xn = [[-1, -1], [-1, 1], [1, -1]]
 mx1 = (xn[0][0] + xn[1][0] + xn[2][0]) / 3
 mx2 = (xn[0][1] + xn[1][1] + xn[2][1]) / 3
 my = (ysrednie[0] + ysrednie[1] + ysrednie[2]) / 3
 
-a1 = (xn[0][0] ** 2 + xn[1][0] ** 2 + xn[2][0] ** 2) / 3
+a1 = (xn[0][0]  2 + xn[1][0]  2 + xn[2][0]  2) / 3
 a2 = (xn[0][0] * xn[0][1] + xn[1][0] * xn[1][1] + xn[2][0] * xn[2][1]) / 3
-a3 = (xn[0][1] ** 2 + xn[1][1] ** 2 + xn[2][1] ** 2) / 3
+a3 = (xn[0][1]  2 + xn[1][1]  2 + xn[2][1]  2) / 3
 a11 = (xn[0][0] * ysrednie[0] + xn[1][0] * ysrednie[1] + xn[2][0] * ysrednie[2]) / 3
 a22 = (xn[0][1] * ysrednie[0] + xn[1][1] * ysrednie[1] + xn[2][1] * ysrednie[2]) / 3
 
